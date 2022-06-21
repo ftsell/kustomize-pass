4405ab7188bd508e6cf91d813afcad8f448d56d4
Finn-Thorben Sell <dev@finn-thorben.me>
2022-06-21 15:25:48 +0200

    differentiate string and binary secret data

--- src/k8s_types/k8s_secret.rs
+++ src/k8s_types/k8s_secret.rs
@@ -20,6 +20,11 @@ pub struct V1Secret {
     /// The serialized form of the secret data is a base64 encoded string, representing the arbitrary (possibly non-string) data value here.
     data: Mapping,
 
+    /// stringData allows specifying non-binary secret data in string form.
+    /// It is provided as a write-only input field for convenience.
+    /// All keys and values are merged into the data field on write, overwriting any existing values.
+    string_data: Mapping,
+
     /// Immutable, if set to true, ensures that data stored in the Secret cannot be updated (only object metadata can be modified).
     /// If not set to true, the field can be modified at any time.
     /// Defaulted to nil.
@@ -36,9 +41,10 @@ impl V1Secret {
     /// Create a new Secret object from the given data and configured constants
     pub fn new(
         metadata: K8sObjectMeta,
-        data: Mapping,
         immutable: Option<bool>,
         secret_type: Option<String>,
+        string_data: Mapping,
+        binary_data: Mapping,
     ) -> Self {
         Self {
             k8s_type_id: K8sTypeId {
@@ -46,9 +52,10 @@ impl V1Secret {
                 kind: "Secret".to_string(),
             },
             metadata,
-            data,
             immutable,
             secret_type,
+            string_data,
+            data: binary_data,
         }
     }
 }
--- src/secret_conversion.rs
+++ src/secret_conversion.rs
@@ -2,14 +2,26 @@ use crate::k8s_types::V1Secret;
 use crate::V1Beta1PassSecret;
 use anyhow::{anyhow, Context};
 use libpass::StoreEntry;
-use serde_yaml::Value;
+use serde_yaml::{Mapping, Value};
 
-fn convert_value(pass_name: &Value) -> anyhow::Result<Value> {
+/// An value that is encoded so that it cane easily be used as a value for Kubernetes Secrets
+#[derive(Debug, Clone, Eq, PartialEq)]
+enum SecretValue {
+    /// Binary base64 encoded content
+    Binary(String),
+
+    /// UTF-8 encoded string content
+    String(String),
+}
+
+fn convert_value(pass_name: &Value) -> anyhow::Result<SecretValue> {
+    // convert arguments to usable format
     let pass_name = match pass_name {
         Value::String(pass_name) => Ok(pass_name),
         _ => Err(anyhow!("Mapping is not a string")),
     }?;
 
+    // retrieve entry from store
     log::debug!("Retrieving {} from pass", &pass_name);
     let pass_entry = match libpass::retrieve(pass_name)
         .context(format!("Could not convert {} to pass secret", pass_name))?
@@ -18,12 +30,17 @@ fn convert_value(pass_name: &Value) -> anyhow::Result<Value> {
         StoreEntry::File(file) => Ok(file),
     }?;
 
-    let result = String::from_utf8(pass_entry.plain_io()?.as_ref().clone())?
-        .strip_suffix('\n')
-        .ok_or_else(|| anyhow!("Could not strip \\n suffix"))?
-        .to_string();
-
-    Ok(Value::String(result))
+    // read and decrypt content from entry
+    let bin_result = pass_entry.plain_io()?.as_ref().to_owned();
+    Ok(match String::from_utf8(bin_result) {
+        Ok(str_result) => SecretValue::String(
+            str_result
+                .strip_suffix('\n')
+                .map(|str_result| str_result.to_string())
+                .unwrap_or(str_result),
+        ),
+        Err(e) => SecretValue::Binary(base64::encode(e.as_bytes())),
+    })
 }
 
 impl TryFrom<V1Beta1PassSecret> for V1Secret {
@@ -44,8 +61,17 @@ impl TryFrom<V1Beta1PassSecret> for V1Secret {
         }
 
         // resolve all pass secrets
-        for (_key, value) in value.data.iter_mut() {
-            *value = convert_value(value)?;
+        let mut str_results = Mapping::new();
+        let mut bin_results = Mapping::new();
+        for (key, value) in value.data.iter_mut() {
+            match convert_value(value)? {
+                SecretValue::String(result) => {
+                    str_results.insert(key.to_owned(), Value::String(result))
+                }
+                SecretValue::Binary(result) => {
+                    bin_results.insert(key.to_owned(), Value::String(result))
+                }
+            };
         }
 
         // construct and return result
@@ -55,9 +81,10 @@ impl TryFrom<V1Beta1PassSecret> for V1Secret {
         );
         Ok(V1Secret::new(
             value.metadata,
-            value.data,
             value.immutable,
             value.secret_type,
+            str_results,
+            bin_results,
         ))
     }
 }
